6eb29b454d038e38f141f476055022f54d741e18
Rebecca Tauber <becky.tauber@ninepts.com>
2016-08-18 15:02:34 -0400

    Switch to PBB_Core

--- reqDeletion.py
+++ reqDeletion.py
@@ -1,11 +1,6 @@
-# TODO: Replace helperFunctions with ProteinBotBoxFunctions.py from diseaseOntology
-import helperFunctions as HF
-import json
+from ProteinBoxBot_Core import PBB_Core
 from ProteinBoxBot_Core import PBB_login
-import pywikibot
 import requests
-import urllib2
-import pprint
 
 __author__ = 'Becky Tauber'
 
@@ -24,9 +19,6 @@ def requestForDeletion(obs_list, pwd, pID, ontology):
     ontology = ontology lookup code
     '''    
     
-    site = pywikibot.Site('test', 'wikidata')
-    # site = pywikibot.Site('wikidata', 'wikidata')
-    
     # TODO: get correct server
     ''' #Uncomment this section for posting!
     #login = PBB_login.WDLogin(user='', pwd=pwd, server='')
@@ -40,36 +32,23 @@ def requestForDeletion(obs_list, pwd, pID, ontology):
     else:
         obs_qids = []
         
-        for o in obs_list:
-            # Format for OLS URL
-            if ":" in o:
-                urlForm = o.replace(":", "_")
-                searchID = o
-                
+        for o in obs_list:   
             # Format for WikiData search
             if "_" in o:
-                urlForm = o
-                searchID = o.replace("_", ":")
-                
-            # get label from ID
-            tUrl = 'http://www.ebi.ac.uk/ols/api/ontologies/{}/terms/http%253A%252F%252Fpurl.obolibrary.org%252Fobo%252F{}'.format(ontology, urlForm)
-            tRequest = urllib2.Request(tUrl)
-            tU = urllib2.urlopen(tRequest)
-            info = tU.read()
-            lib = json.loads(info)
-            label = lib['label']
+                o = o.replace("_", ":")
             
-            wdID = getWikiDataID(site, searchID, label, pID)
+            wdID = getWikiDataID_PBB(o, pID)
             
             if (wdID != None):
                 obs_qids.append(wdID)
             else:
                 pass
-        
+            
+            
         # No obsolete terms in WikiData
         if (len(obs_qids) == 0):
-            pass
             print "No obsolete terms in WikiData"
+            pass
             
         # One term, do deletion request
         if (len(obs_qids) == 1):
@@ -103,25 +82,6 @@ def requestForDeletion(obs_list, pwd, pID, ontology):
             #pprint.pprint(r.json())
             '''
             
-            
-def getNumTerms(ontology):
-    '''
-    Get ontology version information from OLS
-    Get WikiData ID of release, if it exists
-    '''
-    
-    olsUrl = 'http://www.ebi.ac.uk/ols/api/ontologies/{}'.format(ontology)
-    request = urllib2.Request(olsUrl)
-    u = urllib2.urlopen(request)
-    ontology = u.read()
-    lib = json.loads(ontology)
-    
-    
-    numTerms = lib['numberOfTerms']
-    
-    return numTerms
-
-
 
 def postRequest (req_text, login, title):      
     params = {
@@ -138,87 +98,20 @@ def postRequest (req_text, login, title):
     return r
        
        
-def getWikiDataID(site, oboID, label, pID):
+def getWikiDataID_PBB(oboID, pID):
     '''
-    Search WikiData for OBO ID and return the WikiData ID.
-    If no results, check for search by label and return that ID.
-    Double check to make sure an ontology property (pID) is used.
-    Otherwise, return None.
+    Search WikiData for any item that has the claim of given pID
+    (ontology ID property) with the oboID as a string. If no results
+    found, return None.
+    '''   
+    query = 'string[{}:"{}"]'.format(pID.split("P",1)[1], oboID)
+    wd_items = PBB_Core.WDItemList(query).wditems
     
-    Uses the following functions from diseaseOntology/ProteinBoxBotFunctions.py.
-    (Imported as helperFunctions as HF):
-    getItems and claimExists
-    '''
-    # search full wikidata
-    site=pywikibot.Site('wikidata', 'wikidata')
-    wDataByLabel = HF.getItems(site, label)
-    lenByLabel = len(wDataByLabel['search'])
+    if (wd_items['status']['items'] == 1):
+        wdID = 'Q{}'.format(wd_items['items'][0])
     
-    if (oboID != None):
-        wDataByID = HF.getItems(site, oboID)
-        lenByID = len(wDataByID['search'])
-    else:
-        lenByID = 0
-
-    # If there is exactly one hit on ID search, that is the WD ID
-    if (lenByID == 1):
-        wdID = wDataByID['search'][0]['id']
-        
-    # If there are no results...
-    elif (lenByID == 0):
-        # If there is exactly one label result...
-        if (lenByLabel == 1):
-            checkID = wDataByLabel['search'][0]['id']
-            # Check if ontology property is used with OBO ID
-            exists = HF.claimExists(site, checkID, pID, oboID)
-            if exists == True:
-                wdID = checkID
-            # If not, it is not the WD ID
-            else:
-                wdID = None
-        
-        # If there are more than one results for the label search...
-        elif (lenByLabel > 1 and label != 'Thing'):
-            match = []
-            # Find all exact matches
-            for w in wDataByLabel['search']:
-                if w['label'] == label:
-                    match.append(w['id'])
-            
-            # If there is only 1 match...
-            if (len(match) == 1):
-                checkID = wDataByLabel['search'][0]['id']
-                # Check if ontology property is used with OBO ID
-                exists = HF.claimExists(site, checkID, pID, oboID)
-                if exists == True:
-                    wdID = checkID
-                else:
-                    wdID = None
-                
-            # If there are more than 1 matches...
-            elif (len(match) > 1):
-                testID = ''
-                for w in wDataByLabel['search']:
-                    checkID = w['id']
-                    # Check if ontology property is used with OBO ID
-                    exists = HF.claimExists(site, checkID, pID, oboID)
-                    if exists == True:
-                        testID = checkID
-                    else:
-                        pass
-                    
-                if (testID != ''):
-                    wdID = testID
-                else:
-                    wdID = None
-                    
-            else:
-                wdID = None
-        else:
-            wdID = None
     else:
         wdID = None
-    
+        
     return wdID
-    
-    
+  
